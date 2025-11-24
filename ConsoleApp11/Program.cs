2cbb640e444fabb5eb3fec337b91833f024fb4a2
jim <n00484624@upn.pe>
2025-11-24 08:28:48 -0500

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            char[] arreglo;
+            Console.WriteLine("ingrese una palabra");
+            palabra = Console.ReadLine();
+            //convertir la palabra string a un arreglo tipo char
+            arreglo = palabra.ToCharArray();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.WriteLine("[" + arreglo[i] + "]" + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
