a8af45c21a17d8af35f17bbdfdcfd73f25b56c65
jim <n00484624@upn.pe>
2025-11-24 08:41:40 -0500

    Add files via upload
    char

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            char[] arreglo;
+            Console.WriteLine("ingrese su nombre");
+            nombre = Console.ReadLine();
+            arreglo = nombre.ToCharArray();
+            Console.WriteLine("bienvenido :");
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write(arreglo[i]);
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
